8b29a474a32769568c7388c1d473f2fd0ba18aa3
tukli <bhaitoofan21@gmail.com>
2024-11-25 11:49:21 +0530

    Update parity.c

--- parity.c
+++ parity.c
@@ -1,67 +1,59 @@
 #include <stdio.h>
-
+#include <math.h>
 #include <string.h>
+#include <stdlib.h>
+
+void calculate_parity_bits(int data[], int m, int r, int encoded[]) {
+    int i,j = 0, k = 0;
+    for (i = 1; i <= m + r; i++) {
+        if (i == (1 << j)) {
+            encoded[i] = 0; 
+            j++;
+        } else {
+            encoded[i] = data[k]; 
+            k++;
+        }
+    }
+    
+    for (i = 0; i < r; i++) {
+        int parity_pos = (1 << i);
+        int parity_value = 0;
+        for (j = 1; j <= m + r; j++) {
+            if (j & parity_pos) {
+                parity_value ^= encoded[j]; 
+            }
+        }
+        encoded[parity_pos] = parity_value; 
+    }
+}
 
 int main() {
+    int i,data1[] = {1, 0, 0, 1, 1, 0, 1}; 
+    printf("\n enter the data \n");	
+    char data[100];
+	scanf("%s",data);
 
-char binary[100]; int i, count = 0;
+    int m = strlen(data); 
+    int r = 0; 
+    for(r=1;r<m;r++)
+	{
+		if(pow(2,r)>(m+1+r))
+			break;
+	}
+    for(i=0;i<m;i++)
+	{
+		data1[i]=data[i]-48;
+	}
+	int encoded[100] = {0}; 
 
-char parity_choice;
+    calculate_parity_bits(data, m, r, encoded);
 
-// Input binary number as a string printf("Enter a binary number: "); scanf("%s", binary);
+    // Print the encoded data
+    printf("Encoded data: ");
+    for (i = 1; i <= m + r; i++) {
+        printf("%d", encoded[i]);
+    }
+    printf("\n");
 
-// Ask the user for parity type printf("Choose parity even (e) or odd (o): "); scanf("%c", &parity_choice);
-
-// Count number of 1s in the binary string for (i=0; i < strlen(binary); i++) {
-
-if (binary[i] = '1') {
-
-count++;
-
-}
-
-}
-
-// Calculate parity
-
-if (parity_choice == 'e') {
-
-// Even parity
-
-if (count % 2 == 0) {
-
-strcat (binary, "0"); // Append 0 for even parity
-
-} else {
-
-strcat (binary, "1"); // Append 1 for even parity
-
-}
-
-} else if (parity_choice == '0') {
-
-// Odd parity
-
-if (count %2 == 0) {
-
-strcat (binary, "1"); // Append 1 for odd parity
-
-} else {
-
-strcat (binary, "0"); // Append 0 for odd parity
-
-}
-
-} else {
-
-printf("Invalid parity choice!\n");
-
-return 1;
-
-}
-
-// Output the parity encoded binary number printf("Parity encoded data: %s\n", binary);
-
-return 0;
-
+    return 0;
 }
